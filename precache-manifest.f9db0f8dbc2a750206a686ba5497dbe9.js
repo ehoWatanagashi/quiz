85e06d1084ea651d2a9bf3a2553e9b8415f84eb7
Artem Pleshkun <asperatus@inbox.ru>
2020-05-02 18:18:17 +0300

    Updates

---
+++ precache-manifest.f9db0f8dbc2a750206a686ba5497dbe9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f458d30bf8629c3eccd53e7c59e9b21",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "4cda46f06a52dc29fa9b",
+    "url": "/quiz/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fd120d00fe7b5904e395",
+    "url": "/quiz/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4cda46f06a52dc29fa9b",
+    "url": "/quiz/static/js/2.bf11ae29.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/quiz/static/js/2.bf11ae29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd120d00fe7b5904e395",
+    "url": "/quiz/static/js/main.23faeabd.chunk.js"
+  },
+  {
+    "revision": "7e5e903b84d3d404dac3",
+    "url": "/quiz/static/js/runtime-main.bf451133.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/quiz/static/media/persik.4e1ec840.png"
+  }
+]);
